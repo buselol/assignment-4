4ac5e58d7ea09da9dd058c5e0c7f55e82a3a028a
Buse <Buse@DESKTOP-7EVT6O5>
2022-01-16 15:57:03 +0100

    Proje dosyası ekle.

---
+++ ryshyjtrhf/Board.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ryshyjtrhf
+{
+    internal class Board
+    {
+       
+
+
+    
+        
+        
+            internal static readonly char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        internal static bool Player = true;
+        internal static bool Game = true;
+
+        internal const ConsoleColor Back = ConsoleColor.Black;
+        internal const ConsoleColor Color = ConsoleColor.DarkGreen;
+        static void DrawBoard()
+        {
+            Console.WriteLine("Welcome to tictactoe");
+            Console.WriteLine();
+            Console.WriteLine($" {0} | {1} | {2}");
+            Console.WriteLine($"---+---+---");
+            Console.WriteLine($" {3} | {4} | {5}");
+            Console.WriteLine($"---+---+---");
+            Console.WriteLine($" {6} | {7} | {8}");
+            Console.WriteLine();
+        }
+
+    }
+
+}
+
+
---
+++ ryshyjtrhf/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ryshyjtrhf
+{
+  
+
+
+        {
+            internal class Select
+            {
+                internal static void MenuWrite(char Char)
+                {
+                    _ = int.TryParse($"{Char}", out int Value);
+
+                    switch ((Menu)Value - 1)
+                    {
+                        //Menu Content
+                        case MenuType.Game:
+                            Data.Game = false;
+                            Run.Start();
+                            break;
+
+                        case MenuType.About:
+                            Console.Clear();
+                            Console.WriteLine("Greetings, i am buse iyigün from computer science.");
+                            Console.WriteLine();
+                            break;
+
+                        case MenuType.Exit:
+                            Console.WriteLine();
+                            Console.WriteLine("Do you want to quit? [y/n]");
+
+                            Console.Write("= ");
+
+                            if ($"{Print.ReadKey().KeyChar}".ToLowerInvariant() == "y")
+                            {
+                                Environment.Exit(0);
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        default:
+                            Run.Wrong();
+                            break;
+                    }
+                }
+            }
+        }
